8dc3d2d3f665b834900c40beccb1e2921054fd8f
Gaurav Mahawar <thegauravmahawar@gmail.com>
2023-11-11 18:10:09 +0530

    Todo App 2 - Next.js

---
+++ projects/todo-app-2/prisma/migrations/20231111112355_rename_table_todo/migration.sql
@@ -0,0 +1,19 @@
+/*
+  Warnings:
+
+  - You are about to drop the `Todo` table. If the table is not empty, all the data it contains will be lost.
+
+*/
+-- DropTable
+DROP TABLE "Todo";
+
+-- CreateTable
+CREATE TABLE "todo" (
+    "id" TEXT NOT NULL,
+    "title" TEXT NOT NULL,
+    "complete" BOOLEAN NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "todo_pkey" PRIMARY KEY ("id")
+);
