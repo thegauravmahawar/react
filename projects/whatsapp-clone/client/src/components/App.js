b9a16c230bb643ce0319f8e296bcd9eb1d9136bd
Gaurav Mahawar <thegauravmahawar@gmail.com>
2024-03-20 22:44:45 +0530

    Whatsapp clone

---
+++ projects/whatsapp-clone/client/src/components/App.js
@@ -0,0 +1,11 @@
+import React, { useState } from 'react'
+import Login from './Login'
+
+function App() {
+
+  const [id, setId] = useState()
+
+  return <Login onIdSubmit={setId} />
+}
+
+export default App;
