0565dd109d0c767b2c2999993dc8adbccb6b2397
Gaurav Mahawar <thegauravmahawar@gmail.com>
2024-05-19 23:12:11 +0530

    Shopping cart

--- projects/shopping-cart-1/src/App.tsx
+++ projects/shopping-cart-1/src/App.tsx
@@ -1,8 +1,22 @@
+import { Routes, Route } from 'react-router-dom'
+import { Container } from 'react-bootstrap'
+import { About } from './pages/About'
+import { Home } from './pages/Home'
+import { Store } from './pages/Store'
+import { Navbar } from './components/Navbar'
+
 function App() {
 
   return (
     <>
-      <h1>Hi</h1>
+      <Navbar />
+      <Container className='mb-4'>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/store' element={<Store />} />
+          <Route path='/about' element={<About />} />
+        </Routes>
+      </Container>
     </>
   )
 }
